a544852f0fb5d95ad682a6debf870226b8902292
odavidrf <133628217+odavidrf@users.noreply.github.com>
2023-05-15 02:05:53 -0400

    first gitHub

---
+++ src/modules/application/application.js
@@ -0,0 +1,111 @@
+import {
+    indexItemList, 
+    indexNameTask, 
+    indexPriorityTask, 
+    indexTaskList, 
+    indexEditTask, 
+    indexNewPriorityTask, 
+    indexAddButton, 
+    indexDelTask, 
+    indexDelButton,
+    indexEditButton,
+    indexOrderTask
+} from "../index.js";
+
+//clase para crear las tareas
+export class applicationCreate {
+  constructor(name,priority,complete){
+    name = this.name;
+    priority = this.priority;
+    complete = this.complete;
+  }  
+}
+
+//arreglo poara guardar las tareas Creadas
+let applicationSave = []
+
+//creacion de instancia de clase solo como elemento de prueba
+/*const appTask = new applicationCreate (indexNameTask,indexPriorityTask,false);
+applicationSave.push(appTask);*/
+
+//ordenar las tareas segun la prioridad
+const applicationOrder = ()=>{
+
+        applicationSave.sort((a,b)=>{
+
+            return a.priority - b.priority
+
+        });
+
+}
+
+//funcion para eliminar una tarea por nombre
+const DeleteByName = (name)=>{
+//sintax bucar en el arregle = arreglo.filter'filtrar dentro del arreglo'('filtramos accediendo a una propiedad de la clase' hacemos match )
+    applicationSave = applicationSave.filter(applicationCreate => applicationCreate.name !==name);
+
+}
+
+//funcion editar una tarea por nombre
+
+const applicationEditByName = (name, newPriority) =>{
+
+    applicationSave.forEach(applicationCreate =>{
+        if(applicationCreate.name === name){
+            applicationCreate.priority = newPriority
+        };
+    });
+
+}
+
+//Funcion para generar la lista de tareas en el HTML
+const applicationList = ()=>{
+
+    indexTaskList.innerHTML='';
+    applicationSave.forEach(applicationCreate=>{
+        const applicationComplete = applicationCreate.complete ? '(complete)' : '';
+        indexItemList.innerText = `${applicationCreate.name} - Priority: ${applicationCreate.priority}${applicationComplete}`;
+        indexTaskList.appendChild(indexItemList);
+
+//agregar un listener para seleccionar elementos y editar
+        indexItemList.addEventListener('click',()=>{
+
+            indexItemList.addEventListener('click',()=>{
+                indexEditTask.value = applicationCreate.name
+                indexNewPriorityTask.value = applicationCreate.priority });
+
+        });
+    });
+
+}
+
+//agregar un listener para el boton de agregar una tarea (creando instancia de la clase)
+indexAddButton.addEventListener('click',()=>{
+    indexNameTask = applicationCreate.name;
+    indexPriorityTask = applicationCreate.priority;
+    const applicationAddTask = new applicationCreate(indexNameTask,indexPriorityTask,false)
+    applicationSave.push(applicationAddTask)
+    applicationList();
+})
+
+//agregar un listener para el boton de ordenar las tareas
+indexOrderTask.addEventListener('click',()=>{
+    applicationOrder();
+    applicationList();
+})
+
+
+//agregar un listener para el botn eliminar tarea
+indexDelButton.addEventListener('click', ()=>{
+   DeleteByName(indexDelTask.value);
+   applicationList();
+})
+
+//agregar un listener para el boton editar tarea
+indexEditButton.addEventListener('clicl',()=>{
+    applicationEditByName(indexEditTask.value,indexNewPriorityTask.value);
+    applicationList();
+})
+
+
+
---
+++ src/modules/index.js
@@ -0,0 +1,29 @@
+// tomamos la tarea ingresada en la consulta
+
+const nameTask=document.getElementById('mainNaeTask');
+const priorityTask=document.getElementById('mainPriorityTask');
+const TaskList=document.getElementById('mainTaskList');
+const itemList=document.createElement('li');
+const newPriority=document.getElementById('newPriorityTask');
+const addButton=document.getElementById('AddButton');
+const DelButton=document.getElementById('delButton');
+const DelTask=document.getElementById('delTask');
+const EditButton=document.getElementById('editButton');
+const EditTask=document.getElementById('editTask')
+const PriorityOrder=document.getElementById('mainPriorityOrder')
+
+
+export const indexNameTask = nameTask.value;
+export const indexPriorityTask = priorityTask.value;
+export const indexTaskList = TaskList;
+export const indexItemList = itemList;
+export const indexEditTask = EditTask;
+export const indexNewPriorityTask = newPriority;
+export const indexAddButton = addButton;
+export const indexDelButton = DelButton;
+export const indexDelTask = DelTask;
+export const indexEditButton = EditButton;
+export const indexOrderTask = PriorityOrder;
+
+//ahora necesito exponer el codigo segun el archivo application.js 
+
